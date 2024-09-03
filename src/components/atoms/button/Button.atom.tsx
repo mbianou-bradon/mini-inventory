5d725e7ad874223924c961fc55c4a74e1f555dda
mbianou-bradon <mbianoubradon@gmail.com>
2024-09-03 05:40:13 +0100

    fix(component): added extra props to button

--- src/components/atoms/button/Button.atom.tsx
+++ src/components/atoms/button/Button.atom.tsx
@@ -7,11 +7,12 @@ type Props = {
     disable?: boolean;
     onClick?: () => void;
     styles?: string;
+    type?: 'submit' | 'button'
 }
 
-export default function Button({ leftIcon, rightIcon, text, disable, onClick, styles }: Props) {
+export default function Button({ leftIcon, rightIcon, text, disable, onClick, styles, type }: Props) {
     return (
-        <button disabled={disable} onClick={onClick} className='rounded-lg font-bold bg-primary-blue text-white px-6 py-2 active:scale-[0.98] transition-all duration-200'>
+        <button type={type} disabled={disable} onClick={onClick} className={`rounded-lg font-bold bg-primary-blue text-white px-6 py-2 active:scale-[0.98] transition-all duration-200 ${styles}`}>
             <span>{leftIcon}</span><span>{text}</span><span>{rightIcon}</span>
         </button>
     )
