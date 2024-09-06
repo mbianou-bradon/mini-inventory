97be67700e31cbfd79fe8431f76761481577fe57
mbianou-bradon <mbianoubradon@gmail.com>
2024-09-06 10:09:04 +0100

    feat(dropdown): outside handler

--- src/components/atoms/dropdown/Dropdown.atom.tsx
+++ src/components/atoms/dropdown/Dropdown.atom.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import ChevronIcon from '../../../lib/assets/icons/Chevron.icon';
+import UseOnClickOutside from '../../../lib/utils/useonClickOutside.util';
 
 type Props = {
     value?: string;
@@ -8,9 +9,16 @@ type Props = {
     data?: any[];
 }
 export default function Dropdown({ value, placeholder, setValue, data }: Props) {
-    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const ref = useRef(null);
+
+    const outsideClickHandler = useCallback(() => {
+        setIsOpen(false)
+    }, [])
+
+    UseOnClickOutside(ref, outsideClickHandler);
     return (
-        <div className={`relative border border-slate-500 rounded-lg px-4 py-2 w-full outline-none focus:border-primary-blue`}>
+        <div ref={ref} className={`relative border border-slate-500 rounded-lg px-4 py-2 w-full outline-none focus:border-primary-blue`}>
             <p onClick={() => setIsOpen(p => !p)} className={`px-4 text-base text-left cursor-pointer flex items-center justify-between w-full ${value && value !== "" ? 'text-black' : 'text-slate-400'}`}><span>{(value && value !== "") ? data?.filter(p => p.id === value)[0]['nom'] : placeholder}</span><span className={`${!isOpen && "rotate-[180deg]"} transition-all duration-200`}><ChevronIcon /></span></p>
             {isOpen && <ul className='border border-slate-500 rounded-lg absolute left-0 top-[110%] w-full  max-h-[200px] overflow-auto z-20 bg-white'>
                 {data && data.map((item, index) => {
